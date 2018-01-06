baa2c9150958bdbfd88dd5add05c78e19fe06802
Jasonjys <jasonjys0511@gmail.com>
2018-01-05 20:26:10 -0500

    add footer

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ class App extends Component {
                         <Coverpage img={ CoverImg } />
                         <Timeline />
                         <Gallerygrid />
+                        <Footer style={{ textAlign: 'center' }}>
+                            Footer
+                        </Footer>
                     </Content>
                 </Layout>
             </MuiThemeProvider>
