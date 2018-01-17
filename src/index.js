64bff18fb3293e2890bd6f9c07fddc4fe3a6ae0a
Seena Rowhani <seenarowhani95@gmail.com>
2018-01-16 19:25:36 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import './index.css'
+import './index.css';
 
 injectTapEventPlugin();
 ReactDOM.render(
